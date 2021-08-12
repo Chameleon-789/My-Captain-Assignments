20f97b6946f3f86670530d2dabf92b5989c196f4
Chameleon-789 <88381012+Chameleon-789@users.noreply.github.com>
2021-08-12 03:16:47 +0300

    Add files via upload

---
+++ List methods.py
@@ -0,0 +1,31 @@
+x = ["a", "b", "c"]
+
+print(type(x))
+
+x.append("e")
+x.insert(3, "d")
+x.append("J")
+x.append("M")
+
+print(x)
+x.remove("J")
+x.pop(-1)
+
+print(x)
+
+myList1 = ["f", "g"]
+
+x.extend(myList1)
+
+y = x.copy()
+
+x.clear()
+print(y)
+
+print(y.count("d"))
+print(y.index("f"))
+
+y.sort()
+y.reverse()
+
+print(y)
